25c82d74d8111fe529f68fdb5929401f61762cb1
Cherie Williams <cherie@girlgrammer.com>
2013-09-26 12:32:23 -0700

    Updated code for DP2, now working

--- src/display.c
+++ src/display.c
@@ -1,3 +1,13 @@
+// On 9/26/13, switched to DP2 SDK which made a change to gbitmap init and deinit
+//
+// For future reference:
+//
+// OLD: bool gbitmap_init_with_resource(GBitmap* bitmap, int resource_id);
+//			void gbitmap_deinit(GBitmap* bitmap);
+// NEW: GBitmap* gbitmap_create_with_resource( int resource_id );
+//			void gbitmap_destroy( GBitmap *bitmap );
+// 	
+
 #include <pebble_os.h>
 #include <pebble_app.h>
 #include <pebble_fonts.h>
@@ -27,8 +37,8 @@ static TextLayer *title_layer;
 static BitmapLayer *logo_layer;
 static BitmapLayer *icon_layer;
 
-static GBitmap logo_bitmap;
-static GBitmap condition_icons[ NUM_CONDITIONS ];
+static GBitmap *logo_bitmap;
+static GBitmap *condition_icons[ NUM_CONDITIONS ];
 
 /********** FORECAST **********/
 char *getDescription( int value ) {
@@ -44,7 +54,7 @@ char *getDescription( int value ) {
 
 void create_box( char *text, int width_units, int x, int y, int value ) {
 	icon_layer = bitmap_layer_create( GRect( x,y,( width_units * SCREEN_WIDTH )/2,USABLE_HEIGHT/3 ) );
-	bitmap_layer_set_bitmap( icon_layer, &condition_icons[ value ] );
+	bitmap_layer_set_bitmap( icon_layer, condition_icons[ value ] );
 	// unload: called when the window is deinited, but could be used in the future to free resources bound to windows that are not on screen.
   layer_add_child( window_get_root_layer( window ), bitmap_layer_get_layer( icon_layer ) );
 
@@ -93,7 +103,7 @@ static uint16_t menu_get_num_rows_callback( MenuLayer *menu_layer, uint16_t sect
 
 static void menu_draw_row_callback( GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data ) {
 	Location *location = get_location( cell_index->row );
-	menu_cell_basic_draw( ctx, cell_layer, get_spot_name( location ), get_county( location ), &condition_icons[ get_current_conditions( location, OVERALL ) ] );
+	menu_cell_basic_draw( ctx, cell_layer, get_spot_name( location ), get_county( location ), condition_icons[ get_current_conditions( location, OVERALL ) ] );
 }
 
 static void menu_select_callback( MenuLayer *menu_layer, MenuIndex *cell_index, void *data ) {
@@ -102,7 +112,7 @@ static void menu_select_callback( MenuLayer *menu_layer, MenuIndex *cell_index, 
 
 static void menu_load ( Window *window ) {	
 	for (int i = 0; i < NUM_CONDITIONS; i++) {
-		gbitmap_init_with_resource( &condition_icons[ i ], CONDITION_ICONS[ i ] );
+		condition_icons[ i ] = gbitmap_create_with_resource( CONDITION_ICONS[ i ] );
 	}
 
 	menu_layer = menu_layer_create( layer_get_frame( window_get_root_layer( window ) ) );
@@ -121,7 +131,7 @@ static void menu_unload( Window *window ) {
   menu_layer_destroy( menu_layer );
 
 	for (int i = 0; i < NUM_SPOTS; i++) {
-    gbitmap_deinit( &condition_icons[ i ] );
+    gbitmap_destroy( condition_icons[ i ] );
   }
 }
 
@@ -156,8 +166,8 @@ static void splash_load( Window *window ) {
 	layer_add_child( window_get_root_layer( window ), text_layer_get_layer( title_layer ) );
 	
 	logo_layer = bitmap_layer_create( GRect( 0, USABLE_HEIGHT/2, SCREEN_WIDTH, 40 ) );
-  gbitmap_init_with_resource( &logo_bitmap, RESOURCE_ID_SPLASH_ICON );
-	bitmap_layer_set_bitmap( logo_layer, &logo_bitmap );
+  logo_bitmap = gbitmap_create_with_resource( RESOURCE_ID_SPLASH_ICON );
+	bitmap_layer_set_bitmap( logo_layer, logo_bitmap );
   layer_add_child( window_get_root_layer( window ), bitmap_layer_get_layer( logo_layer ) );
 	
 	text_layer = text_layer_create( GRect( 0,USABLE_HEIGHT-( 2*STATUS_BAR_HEIGHT ),SCREEN_WIDTH,STATUS_BAR_HEIGHT*2 ) );
@@ -174,7 +184,7 @@ static void splash_load( Window *window ) {
 
 static void splash_unload( Window *window ) {
 	layer_destroy( window_get_root_layer( window ) );
-	gbitmap_deinit( &logo_bitmap );
+	gbitmap_destroy( logo_bitmap );
 }
 
 static void create_splash_display( void ) {	
--- src/surfcast.c
+++ src/surfcast.c
@@ -10,13 +10,6 @@
 #include "app_msg.h"
 #include "display.h"
 
-#define MY_UUID { 0x55, 0xFF, 0x55, 0x62, 0x68, 0xBF, 0x4B, 0xF8, 0x83, 0xD4, 0x44, 0xE6, 0xB0, 0x9E, 0x0D, 0x59 }
-PBL_APP_INFO( MY_UUID,
-             "Pebble Surfcast", "Girlgrammer",
-             1, 0, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP );
-
 static void init( void ) {
 	init_windows();
 	app_message_init();
